2de7356979e2bc90402d7047b0a41d818ee24cd6
CumAss <habibmazidov5@gmail.com>
2022-12-01 18:23:31 +0500

    asd

---
+++ JS/main.js
@@ -0,0 +1,87 @@
+// 1.[ let, var, const.:
+// let-ozgaruvchan ozgaruvchi
+// var-ozgaruvchan ozgaruvchi
+// const-ozgarmas ozgaruvchi
+// ]
+
+// 2.[
+// string
+// number
+// undefined
+// boolean
+// Object
+// null
+// Symbol
+//]
+
+
+
+// 3.[
+// Increment = (++) ,
+// decrement (--)
+//]
+
+
+
+// 4.[
+// string + numbr = string
+//]
+
+
+// 5.[
+// && = (va belgisi)
+// || (yokiiy)
+//]
+
+// 6.[
+// if = (agar)
+// else = (yokiiy)
+//]
+
+
+// 7.[
+// if(a < b){
+//    console.log('hello world');
+//}
+// else if(a < c){
+// console.log('salom');
+//}
+//]
+
+//8 [
+// massiv = 'massiv ozid malumotlarni tartib soni boyicha saqlaydi yaniyy index'
+// ]
+
+// 9. [
+// kalit sozlar yordamida saqlanadi
+// ]
+
+// 10. [
+// massivni (method-lari. push, toUpperCase, toLowerCase, lenght, map, shift, unshift)
+//]
+
+
+// Massalalar
+
+// 1-Masala[
+// let asd = (a, b) => {
+//     console.log(a + b);
+// }
+// asd(35, 22)
+//]
+
+//2-Masala
+// let b = 2022
+// function asd(a){
+// return b - a
+// }
+// console.log(asd(12));
+
+// const asd = (a, b) => {
+//     if (typeof a ==="number" && typeof b ==="number") {
+//         console.log(a + b);
+//     } else {
+//         console.log("son emas");
+//     }
+// }
+// asd(1, 2)
